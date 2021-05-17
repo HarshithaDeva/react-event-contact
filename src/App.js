425c34a18f7f50f25ddb29cabded8ace69d614de
HarshithaDeva <81804023+HarshithaDeva@users.noreply.github.com>
2021-05-17 12:36:46 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Contactus from "./components/Contactus";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      
+      <Contactus/>
+    </div>
+  );
+}
+
+export default App;
